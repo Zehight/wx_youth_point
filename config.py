bc843eeaddea58b77a8b0dc09893e4d2dc3eae7b
毛欣鑫 <1318141712@qq.com>
2023-10-20 13:51:36 +0800

    ok

--- config.py
+++ config.py
@@ -7,11 +7,13 @@ dev_config = {
     'SecretId': 'AKIDeDPTO9i7RitzgVJwYK1a0DIDze5d6Rlt',
     'SecretKey': '543z8DNeprhPzMUCi2GKDEG94rW6Wisv',
     'data_base': 'vote',
-    'app_id': 'wxbd53b7860f2c9844',
-    'app_secret': '77290c01bd98b4382a9870a2f90785ec',
+    'app_id': 'wx4f2dbbae1029571a',
+    'app_secret': '8f5de26f06c3c42e5eec051738b422b9',
     'dev': True,
     'img_bucket_name': 'vote-img-1259115987',
-    'small_img_bucket_name': 'vote-small-img-1259115987'
+    'small_img_bucket_name': 'vote-small-img-1259115987',
+    'cos_bucket':'7072-prod-4gjlstdw9f5f0d7d-1318978931',
+    'cos_region':'ap-shanghai'
 }
 
 now_config = dev_config
@@ -22,10 +24,18 @@ password = os.environ.get("MYSQL_PASSWORD", now_config['password'])
 db_address = os.environ.get("MYSQL_ADDRESS", now_config['db_address'])
 data_base = os.environ.get("DATA_BASE", now_config['data_base'])
 API_GATEWAY = os.environ.get("API_GATEWAY", '')
+
+# 腾讯云cos
 SecretId = os.environ.get("secret_id", now_config['SecretId'])
 SecretKey = os.environ.get("secret_key", now_config['SecretKey'])
+
 app_id = os.environ.get("APP_ID", now_config['app_id'])
 app_secret = os.environ.get("APP_SECRET", now_config['app_secret'])
+
+# 微信云托管cos
+cos_bucket = os.environ.get("COS_BUCKET",now_config['cos_bucket'])
+cos_region = os.environ.get("COS_REGION",now_config['cos_region'])
+
 dev = os.environ.get("DEV", now_config['dev'])
 img_bucket_name = os.environ.get("IMG_BUCKET_NAME", now_config['img_bucket_name'])
 small_img_bucket_name = os.environ.get("SMALL_IMG_BUCKET_NAME", now_config['small_img_bucket_name'])
