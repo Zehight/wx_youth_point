d9132f1ee3d67c2a3913465a9e04822609e98851
毛欣鑫 <1318141712@qq.com>
2023-12-01 14:13:59 +0800

    评论fix

--- application/comment/function.py
+++ application/comment/function.py
@@ -1,6 +1,6 @@
 from sqlalchemy.exc import IntegrityError
 
-from service.models import Comment
+from service.models import Comment,User
 
 
 # 新增
@@ -40,15 +40,35 @@ def getinfo_func(**kwargs):
         return "操作失败", '数据不存在'
 
 
+def getUser(user_id):
+    user = User.get(id=user_id)
+    return user.to_dict()['nike_name']
+
 # 分页查询列表
 def getlist_func(**kwargs):
     result = Comment.search(**kwargs)
+    for reply in result['list']:
+        reply['create_by_name'] = getUser(reply['create_by'])
+        if reply['reply_id'] is not None:
+            reply['reply_user_name'] = getUser(reply['reply_id'])
+        else:
+            reply['reply_user_name'] = ''
     return "操作成功", result
 
 
 def get_list_by_activity(**kwargs):
     result = Comment.search(**kwargs)
     for main_reply in result['list']:
-        follow_reply = Comment.search(comment_main_id=main_reply['id'], page=1, rows=3)
-        main_reply['reply'] = follow_reply['list']
+        main_reply['create_by_name'] = getUser(main_reply['create_by'])
+        main_reply['reply_user_name'] = ''
+        follow_reply_list = Comment.search(comment_main_id=main_reply['id'], page=1, rows=3)
+        for follow_reply in follow_reply_list['list']:
+            follow_reply['create_by_name'] = getUser(follow_reply['create_by'])
+
+            if follow_reply['reply_id'] is not None:
+                follow_reply['reply_user_name'] = getUser(follow_reply['reply_id'])
+            else:
+                follow_reply['reply_user_name'] = ''
+
+        main_reply['reply'] = follow_reply_list['list']
     return "操作成功", result
