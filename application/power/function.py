3d49064d4d8edabbd8915551da13ca3a91c1bd8d
毛欣鑫 <1318141712@qq.com>
2024-03-25 09:20:37 +0800

    改为百度

--- application/power/function.py
+++ application/power/function.py
@@ -5,20 +5,31 @@ import re
 import code
 answers = {}
 
+def get_access_token():
+    url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=P9rOek0jMTCDYKH2aTrtLf7Q&client_secret=lnIqB5SfNWz4excOO01JATLekRUyNkSK"
+    payload = json.dumps("")
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json().get("access_token")
+
 def genStreamAnswer(question, id):
     answers[id] = ''
     payload = json.dumps({
         "messages": question
     })
-    response = requests.post(f"https://gpt.miragari.com/pro/stream/ask",payload,stream=True)
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token=" + get_access_token()
+    response = requests.post(url,payload,stream=True)
     for chunk in response.iter_lines():
         chunkStr = chunk.decode('utf-8')
-        if chunkStr.startswith('data:{'):
+        if chunkStr.startswith('data: {'):
             # chunkJson = json.loads(chunkStr[6:])
-            regex = re.compile(r'{"content": "(.*?)"}')
+            regex = re.compile(r'"result":"(.*?)"')
             result = re.findall(regex,chunkStr)
             if len(result) >0:
-                 answers[id] += result[0].encode('utf-8').decode('unicode_escape')
+                 answers[id] += result[0]
     answers[id] = '完成：' + answers[id]
             # if type(chunkJson) == dict:
             #     chunkRes = chunkJson['choices'][0]['delta']
