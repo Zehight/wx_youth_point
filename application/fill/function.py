98cad199a4972720454cf8a5106b573e7ed055fb
毛欣鑫 <1318141712@qq.com>
2024-07-25 17:57:14 +0800

    ok

--- application/fill/function.py
+++ application/fill/function.py
@@ -8,42 +8,19 @@ from application.user import function as UserFuncs
 
 # 新增
 def create_func(**kwargs):
-    kwargs['create_by'] =
-    user_code = kwargs['user_code']
-    real_name = kwargs['real_name']
-    dept_id = kwargs['dept_id']
-    open_id = kwargs['open_id']
-    if open_id == '':
-        return "操作失败","非法请求"
+    kwargs['create_by'] =kwargs['open_id']
+    Fill.create(**kwargs)
+    return "操作成功","添加成功"
 
-    # 获取绑定信息和用户信息
-    binding = Binding.get(open_id=open_id)
-
-    if binding:
-        user = User.get(id=binding.user_id)
-        Fill.create(book_name=kwargs['book_name'], create_by=user.id)
-        return "操作成功", "数据添加成功"
+def info_func(**kwargs):
+    fill = Fill.get(create_by=kwargs['open_id'])
+    if fill:
+        return "操作成功", fill.to_dict()
     else:
-        request_user = User.get(user_code=user_code)
-        if request_user:
-            return "操作失败", "用户已绑定"
-        user=User.create(real_name=real_name,dept_id=dept_id,user_code=user_code)
-        user_id = user['id']
-        Binding.create(open_id=open_id,user_id=user_id)
-        msg,data = UserFuncs.getToken_func(open_id=open_id)
-        return "token",data
-
+        return "操作失败", '数据不存在'
 
 # 更新
 def update_func(**kwargs):
-    fill = Fill.get(id=kwargs['id'])
-    return_time = datetime.now()
-    kwargs['return_time'] = return_time
+    fill = Fill.get(create_by=kwargs['open_id'])
     fill.update(**kwargs)
     return "操作成功","数据修改成功"
-
-
-# 分页查询列表
-def getlist_func(**kwargs):
-    result = Fill.search(**kwargs)
-    return "操作成功",result
