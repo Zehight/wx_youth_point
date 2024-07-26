85df38a542035933e743285306f66abd5283a595
毛欣鑫 <1318141712@qq.com>
2024-07-26 15:28:01 +0800

    ok

--- application/fill/function.py
+++ application/fill/function.py
@@ -23,5 +23,6 @@ def info_func(**kwargs):
 # 更新
 def update_func(**kwargs):
     fill = Fill.get(create_by=kwargs['open_id'])
+    del kwargs['open_id']
     fill.update(**kwargs)
     return "操作成功","数据修改成功"
--- application/fill/router.py
+++ application/fill/router.py
@@ -9,18 +9,18 @@ from service.login import token_required
 
 @fill.route('/add', methods=['POST'])
 def add():
-    open_id = request.headers.get("x-wx-openid", "")
+    # open_id = request.headers.get("x-wx-openid", "")
     requestData = json.loads(request.data)
-    requestData['open_id'] = open_id
+    # requestData['open_id'] = open_id
     msg, data = FillFuncs.create_func(**requestData)
     return MyResponse.make_succ_response(msg=msg, data=data)
 
 
 @fill.route('/info', methods=['POST'])
 def info():
-    open_id = request.headers.get("x-wx-openid", "")
+    # open_id = request.headers.get("x-wx-openid", "")
     requestData = json.loads(request.data)
-    requestData['open_id'] = open_id
+    # requestData['open_id'] = open_id
     msg, data = FillFuncs.info_func(**requestData)
     return MyResponse.make_succ_response(msg=msg, data=data)
 
@@ -28,8 +28,8 @@ def info():
 
 @fill.route('/update', methods=['POST'])
 def update():
-    open_id = request.headers.get("x-wx-openid", "")
+    # open_id = request.headers.get("x-wx-openid", "")
     requestData = json.loads(request.data)
-    requestData['open_id'] = open_id
+    # requestData['open_id'] = open_id
     msg, data = FillFuncs.update_func(**requestData)
     return MyResponse.make_succ_response(msg=msg, data=data)
